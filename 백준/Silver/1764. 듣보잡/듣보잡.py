a837cdaf08beab93b75884e6312e0cab07d097f4
SuJeKim <127725816+SuJeKim@users.noreply.github.com>
2024-03-14 17:29:23 +0900

    [Silver IV] Title: 듣보잡, Time: 3868 ms, Memory: 43924 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,20 +1,10 @@
 n, m = map(int, input().split())
-heard = sorted([input().strip() for _ in range(n)])
-seen = sorted([input().strip() for _ in range(m)])
+heard = {input().strip() for _ in range(n)}
+seen = {input().strip() for _ in range(m)}
 
-def binary_search(array, target):
-    start, end = 0, len(array) - 1
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] == target:
-            return True
-        elif array[mid] < target:
-            start = mid + 1
-        else:
-            end = mid - 1
-    return False
+# 교집합을 이용하여 듣도 보도 못한 사람의 명단을 얻음
+result = sorted(list(heard & seen))
 
-result = [name for name in heard if binary_search(seen, name)]
 print(len(result))
-for name in sorted(result):
+for name in result:
     print(name)
