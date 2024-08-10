44242546c7f49427ae1f3ec428fd044ded4077b9
SuJeKim <127725816+SuJeKim@users.noreply.github.com>
2024-08-10 21:46:02 +0900

    [Silver II] Title: 예산, Time: 48 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,7 +1,6 @@
 N = int(input())
 lst = list(map(int, input().split()))
 M = int(input())
-result = 0  # 출력해야 하는 최대 예산
 start, end = 1, max(lst)  # 1을 시작, 최댓값을 끝
 while start <= end:
     mid = (start + end) // 2  # 중앙 원소 고르기
@@ -12,7 +11,6 @@ while start <= end:
         else:
             total += l
     if total <= M:  # M 이하 이면 중앙값+1 ~ 끝 값 다시 찾기
-        result = mid
         start = mid + 1
     else:  # M초과 이면 시작 ~ 중앙값-1 값 다시 찾기
         end = mid - 1
