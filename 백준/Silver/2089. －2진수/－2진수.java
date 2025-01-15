8e5f4f450518bd542110d7e4af4f000ca823350f
SuJeKim <127725816+SuJeKim@users.noreply.github.com>
2025-01-15 15:20:42 +0900

    [Silver II] Title: -2진수, Time: 168 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Silver/2089. －2진수/－2진수.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int n = sc.nextInt();
+		if(n == 0) {
+			sb.append(n);
+		}else {
+			while(n != 1) {
+				sb.append(Math.abs(n % (-2)));
+				
+				//N을 재설정 ⇒ 몫을 올림
+				n = (int)Math.ceil((double)n / (-2));
+			}
+			sb.append(n);
+		}
+		System.out.println(sb.reverse());
+
+	}
+
+}
