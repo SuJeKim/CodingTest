97777bd49e1e728dc1ca68e661aeca33dc8c41da
SuJeKim <127725816+SuJeKim@users.noreply.github.com>
2025-03-05 22:42:55 +0900

    [Silver I] Title: 동물원, Time: 112 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -1,31 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
+	private static final int mod = 9901;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 		
-		final int mod = 9901;
-		
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		
-		/**
-		 * dp[n+1][3] 선언
-		 * i = i번쨰 열, j = 우리에 넣는 경우의 수
-		 * 0 = 배치 안함, 1 = 첫번째 칸, 2 = 둘째 칸
-		 */
 		int[][] dp = new int[n+1][3];
 		dp[1][0] = dp[1][1] = dp[1][2] = 1;
 		
-		for(int i = 2; i <= n; i++) {
+		for(int i = 2; i<=n; i++) {
 			dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % mod;
-			dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % mod;
+			dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % mod; 
 			dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % mod;
 		}
-		
-		int answer = (dp[n][0] + dp[n][1] + dp[n][2]) % mod;
-		System.out.println(answer);
-
+		int result = (dp[n][0] + dp[n][1] + dp[n][2]) % mod;
+		System.out.println(result);
 	}
 
 }
