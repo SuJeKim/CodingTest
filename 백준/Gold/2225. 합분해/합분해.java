75a3c4c54df46853fa4170b90379901cd0f2b9a5
SuJeKim <127725816+SuJeKim@users.noreply.github.com>
2025-03-07 22:49:10 +0900

    [Gold V] Title: 합분해, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -5,32 +5,30 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	private static final int MOD =  1000000000;
 
+	static final int mod = 1_000_000_000;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		// 입력
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 		
-		// dp[][] 선언 및 k = 1, n =0 1로 초기화
-		int[][] dp = new int[k+1][n+1];
-		Arrays.fill(dp[1] ,1);
-		for(int i = 1; i <= k; i++){
-			dp[i][0] = 1;
+		int[][] dp = new int[n+1][k+1];
+		
+		Arrays.fill(dp[0], 1);
+		for(int i = 0; i <= n; i++) {
+			dp[i][1] = 1;
 		}
 		
-		// dp 구하기
-		for(int i = 2; i <=k; i++) {
-			for(int j =1; j <=n; j++) {
+		for(int i = 1; i <= n; i++) {
+			for(int j = 2; j <= k; j++) {
 				dp[i][j] = dp[i-1][j] + dp[i][j-1];
-				dp[i][j] %= MOD;
+				dp[i][j] %= mod;
 			}
 		}
-		System.out.println(dp[k][n]);
+		System.out.println(dp[n][k]);
 	}
 
 }
