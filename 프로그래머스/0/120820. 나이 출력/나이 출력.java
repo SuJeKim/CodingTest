31efeb450c53878333946f6db6ed9a794558aebd
SuJeKim <127725816+SuJeKim@users.noreply.github.com>
2025-04-18 21:54:24 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+import java.time.*;
+
+class Solution {
+    public int solution(int age) {
+        int year = 2022;
+        
+        int answer = (year - age) + 1;
+        return answer;
+    }
+}
