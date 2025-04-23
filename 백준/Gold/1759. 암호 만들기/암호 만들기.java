7a1e92b8ce59756d84f044f1307d360b947d1480
SuJeKim <127725816+SuJeKim@users.noreply.github.com>
2025-04-23 22:32:21 +0900

    [Gold V] Title: 암호 만들기, Time: 104 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -2,62 +2,67 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int L, C;
-    static char[] code;
-    static char[] arr;
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	static int L,C;
+	static char[] arr,code;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		L = Integer.parseInt(st.nextToken()); 
+		C = Integer.parseInt(st.nextToken());
+		
+		// 입력값 저장 배열
+		arr = new char[C];
+		// 출력할 배열
+		code = new char[L];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i<C; i++) {
+			arr[i] = st.nextToken().charAt(0);
+		}
+		Arrays.sort(arr);
+		dfs(0,0);
+		System.out.println(sb);
+	}
 
-        L = Integer.parseInt(st.nextToken()); // 암호의 길이
-        C = Integer.parseInt(st.nextToken()); // C개의 소문자
+	private static void dfs(int depth, int start) {
+		if(depth == L) {
+			if(check(code)) {
+				for(char val : code) {
+					sb.append(val);
+				}
+				sb.append("\n");
+			}
+			return;
+		}
+		for(int i = start; i < C; i++) {
+			code[depth] = arr[i];
+			dfs(depth + 1, i + 1);
+		}
+		
+	}
 
-        code = new char[L]; // 암호를 저장할 배열
-        arr = new char[C]; // 알파벳 입력받을 배열
+	// 모음 한개, 자음 2개 구성 확인하는 함수
+	private static boolean check(char[] code) {
+		int vo = 0; // 모음 개수
+		int co = 0; // 자음 개수
+		
+		for(char ch : code) {
+			if(ch == 'a' || ch == 'i' || ch == 'o' || ch == 'e' ||
+					ch == 'u') {
+				vo++;
+			}else {
+				co++;
+			}
+		}
+		if(vo >= 1 && co >= 2) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < C; i++) {
-            arr[i] = st.nextToken().charAt(0); // 초기화
-        }
-        Arrays.sort(arr); // 사전순으로 정렬
-
-        dfs(0, 0); //로직 실행
-    }
-
-    public static void dfs(int depth, int start) {
-        // depth 가 L일 경우 종료
-        if (depth == L) {
-            if (isValid(code)) { // 자음,모음 체크하는 로직 실행 후 true 일 경우에만 출력
-                for (char c : code) {
-                    System.out.print(c);
-                }
-                System.out.println();
-            }
-            return;
-        }
-        for (int i = start; i < C; i++) {
-            code[depth] = arr[i]; // 코드배열 입력
-            dfs(depth+1, i+1); // depth 한칸 깊이, start 위치 한칸 이동
-        }
-    }
-
-    // 자음이 2개이상 모음이 1개 이상인지 판별하는 로직
-    public static boolean isValid(char[] code) {
-        int c = 0; // 자음의 수
-        int v = 0; // 모음의 수
-        for (int i = 0; i < code.length; i++) {
-            if (code[i] == 'a' || code[i] == 'e' || code[i] == 'i' || code[i] == 'o' || code[i] == 'u') {
-                v++; // 모음일 경우 v++
-            } else {
-                c++; // 자음일 경우 c++
-            }
-        }
-        if (c >= 2 && v >= 1) {
-            return true; // 자음 2개 이상, 모음 1개이상 일 경우 true 반환
-        } else {
-            return  false; // 아닐경우 false 반환
-        }
-    }
 }
