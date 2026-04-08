05c8af2b29f76c970997dd67c2fdfba003b0cb85
SuJeKim <127725816+SuJeKim@users.noreply.github.com>
2026-04-08 10:17:52 +0900

    [level 1] Title: 내적, Time: 0.05 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -1,8 +1,9 @@
 class Solution {
     public int solution(int[] a, int[] b) {
-        int sum = 0;
-        for (int i = 0; i < a.length; i++) {
-            sum += a[i] * b[i];
+       int sum = 0;
+        
+        for(int i =0; i < a.length; i++){
+            sum += a[i]*b[i];
         }
         return sum;
     }
